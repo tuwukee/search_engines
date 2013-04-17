2d054e22a522024bddd08dfc9a9044f60db3828f
Yulia Oletskaya <voletsky@gmail.com>
2013-04-17 14:17:11 +0300

    hightlighting for thinking sphinx

--- app/controllers/sphinx_results_controller.rb
+++ app/controllers/sphinx_results_controller.rb
@@ -1,5 +1,10 @@
 class SphinxResultsController < ActionController::Base
   def index
     @results = Goof.search params[:search]
+    @excerpter = ThinkingSphinx::Excerpter.new 'goof_core', params[:search], {
+      :before_match    => '<span class="match" style="background-color: yellowgreen;">',
+      :after_match     => '</span>',
+      :chunk_separator => ' &#8230; '
+    }
   end
 end
